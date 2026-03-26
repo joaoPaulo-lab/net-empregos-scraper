eccdd3247674aced2292baf2a87de01cc03415be
Joao Santos <joaopaulosantos.21@outlook.com>
2026-03-26 05:08:33 +0000

    Refatoração para legibilidade
    Código mais legível com isolamento de responsabilidades.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # net-empregos-scrapper
 Scrapper de dados do site net-empregos que retorna JSON's
 
+O programa acede ao website, pergunta ao utilizador os parametros da busca e faz um scraping das vagas.
+
+Atenção: Favor não utilizar para pesquisas grandes e em seguida, isto pode prejudicar o tráfego do próprio website, já que o scraper acede aos links de forma muito rápida para obter as informações.
+
 Código para iniciar: node net-empregos-scrapper.mjs
--- src/net-empregos-scrapper.mjs
+++ src/net-empregos-scrapper.mjs
@@ -7,11 +7,11 @@ import { stdin as input, stdout as output } from 'process';
 
 //É necessário fazer duas vezes o scrapping então fiz uma função
 //Faz scraping do texto dos options e valor das prop. values
-async function dropBoxScrapper(formCategorias) {
+async function dropBoxScrapper(dropBox) {
     let optionsAndValues = []
     let i = 0;
     //Scraping das opções de Categorias
-    for (const option of await formCategorias.locator('option').all()) {
+    for (const option of await dropBox.locator('option').all()) {
 
         let titulo = await option.textContent();
         let valor = await option.getAttribute('value');
@@ -27,7 +27,7 @@ async function dropBoxScrapper(formCategorias) {
 }
 
 function mostrarLista(itens){
-    //Desconstroi para separar a chave index das outras duas
+   
     for (let item of itens) {
         console.log(`${item.index} - ${item.titulo}`);
     }
@@ -44,9 +44,11 @@ async function fazerPergunta(pergunta){
 
 async function pegarLinks(page) {
     let linkVagas = [];
+    
 
+    await page.waitForSelector('.job-item');
     const jobs = page.locator('.job-item');
-    console.log(jobs);
+
     let quantidadeVagas = await jobs.count();
     console.log(quantidadeVagas + ' vagas encontradas na página atual.');
 
@@ -57,7 +59,7 @@ async function pegarLinks(page) {
         const job = await jobs.nth(i);
         
         try {
-
+            await page.waitForSelector('a.oferta-link');
             let href = await job.locator('a.oferta-link').first().getAttribute('href');
             
             linkVagas.push(href);
@@ -66,37 +68,26 @@ async function pegarLinks(page) {
 
             console.log(`Somente ${i} vagas atendem os parametros.`)
             break;
-            
+
         }
     }
     return linkVagas;
 }
 
-async function run() { 
+async function definirPesquisa(page){
 
-    const browser = await chromium.launch({ headless: false })
-    const context = await browser.newContext();
-    const page = await context.newPage();
-
-    console.log('A aceder website...')
-    await page.goto('https://www.net-empregos.com/');
-    
-    //Remove as perguntas iniciais ao abrir o site.
-    await page.getByRole('button', { name: 'Permitir seleção' }).click();
-    await page.getByRole('button', { name: 'Não Ativar' }).click();
-
-    //Variaveis a serem escolhidas pelo utilizador
     const formCategorias = page.locator('#categoria');
     const formZonas = page.locator('#zona');
     const formChave = page.locator('#chaves');
-
     
     //Pega a lista de categorias e zonas
     let categorias = await dropBoxScrapper(formCategorias);
     let zonas = await dropBoxScrapper(formZonas);
     
+    //PERGUNTAS
     console.log('Categorias');
     mostrarLista(categorias);
+
 
     const escolhaCategoria = await fazerPergunta('Escolha a categoria pelo index: ')
     //Separa o objeto escolhido.
@@ -107,86 +98,70 @@ async function run() {
     console.log('Zonas');
     mostrarLista(zonas);
     
+
+    
     const escolhaZona = await fazerPergunta('Escolha a zona pelo index: ')
     //Separa o objeto escolhido.
     const zonaEscolhida =  zonas.find(zona=>zona.index == parseInt(escolhaZona))
-
+    
     const escolhaChave = await fazerPergunta('Escolha uma palavra chave ex: Suporte, Vendas: ')
     let chave = (await escolhaChave).replaceAll(' ','+')
-
-
-    console.log(`A procurar vagas de ${categoriaEscolhida.titulo} com ${escolhaChave} na zona de ${zonaEscolhida.titulo}...`);
     
+ 
     
-    
+    console.log(`A procurar vagas de ${categoriaEscolhida.titulo} com ${chave} na zona de ${zonaEscolhida.titulo}...`);
     
     await formChave.fill(escolhaChave);
     await formCategorias.selectOption(categoriaEscolhida.valor);
     await formZonas.selectOption(zonaEscolhida.valor);
-    
-    await page.getByRole('button', { name: ' Pesquisar' }).click();
-    await page.pause();
 
-    //Pega os links da pagina inical
-    
-    let linkVagas = [...await pegarLinks(page)]
-    //pega o texto da paginação e a quantidade de vagas para descobrir quantas paginas foram retornadas
-    const frasePaginacao = await page.getByRole('heading', { name: 'ofertas encontradas' }).textContent()
+    return {chave:chave, zonaEscolhida: zonaEscolhida.valor, categoriaEscolhida: categoriaEscolhida.valor }
+}
 
-    // console.log(frasePaginacao.split(' '))
+async function pegaLinksPorPagina(context,chave,categoriaEscolhida,zonaEscolhida,totalPaginas){
+    //Loop começa na pagina 2 
 
-    //A frase de paginação só inclui este caractere caso hajam multiplas paginas
-    if(frasePaginacao.includes('-')){
-        
-          
+    let linksPorPagina = []
+        for (let i = 2; i <= totalPaginas; i++) {
 
-        let paginacaoSeparada = frasePaginacao.split('-')[1].split(' ');
-        for (let i = 2; i <= paginacaoSeparada[4]; i++) {
-
-            //Loop começa na pagina 2 
-            console.log(`Acessando página ${i} de ${paginacaoSeparada[4]}`);
+            console.log(`Acessando página ${i} de ${totalPaginas}`);
 
             
-            let pesquisaPorUrl = `https://www.net-empregos.com/pesquisa-empregos.asp?page=${i}&chaves=${chave}&cidade=&categoria=${categoriaEscolhida.valor}&zona=${zonaEscolhida.valor}&tipo=0`;  
+            let pesquisaPorUrl = `https://www.net-empregos.com/pesquisa-empregos.asp?page=${i}&chaves=${chave}&cidade=&categoria=${categoriaEscolhida}&zona=${zonaEscolhida}&tipo=0`;  
             //abre uma nova pagina
             let newP = await context.newPage();
             try {
                 await newP.goto(pesquisaPorUrl);
                 //Desconstroi o array que recebe e insere os valores no array linkVagas
-                linkVagas.push(...await pegarLinks(newP));
+                linksPorPagina.push(...await pegarLinks(newP));
                 
             } catch (error) {
                 console.log(` página ${i} falhou, pulando para a proxima`)
-                continue;
+            } finally{
+                await newP.close()
             }
 
-            await newP.close()
         }
+    return linksPorPagina;
+}
 
-    }
-    else {
-        console.log('Somente uma pagina foi encontrada');
-        
-    }
-
-    
-    //Faz um loop pelas paginas e coleta os links
-
-   
-    
-    console.log('Links coletados:', linkVagas);
-    console.log('Total de links coletados:', linkVagas.length); 
-
-    //Isto sera feito por vaga...
+async function acederVagasEGravarConteudos(context,linkVagas) {
     for (let i = 0; i < linkVagas.length; i++) {
+
+        let novaUrl = 'https://www.net-empregos.com' + linkVagas[i];
+        
+        const newPage = await context.newPage()
 
         try {
             console.log(`Acessando vaga ${i + 1} de ${linkVagas.length}`);
-            let ref = linkVagas[i].split('/')[1]
-            
-            let novaUrl = 'https://www.net-empregos.com' + linkVagas[i];
-            
-            const newPage = await context.newPage()
+            let partes = linkVagas[i].split('/')
+
+            let infoVagaUrl = {
+                ref: partes[1],
+                nome: partes[2]
+            }
+
+
             await newPage.goto(novaUrl);
         
             let dadosJob = await newPage.locator('script[type="application/ld+json"]').textContent();
@@ -199,24 +174,91 @@ async function run() {
             .replace(/<br\s*\/?>/gi, '\\n')
             .trim();
             
-            const jsonJob = JSON.parse(dadosJob);
             
             //Todo, decidir o que fazer com os Jsons...
+            // const jsonJob = JSON.parse(dadosJob);
 
-            fs.writeFileSync(`../data/vaga_${linkVagas[i].split('/')[1]}_${linkVagas[i].split('/')[2]}.json`, dadosJob);
-            await newPage.close();
+            fs.writeFileSync(`../data/vaga_${infoVagaUrl.ref}_${infoVagaUrl.nome}.json`, dadosJob);
+
+            
         } catch (error) {
 
             console.log(error);
             console.log('Scraping da vaga falhou, passando para a próxima. ')
-            await newPage.close();
+            
             continue;
 
+        } finally
+        {
+            await newPage.close();
         }
           
         
-    }   
+    }
+}
+
+
+async function run() { 
+    
+    const browser = await chromium.launch({ headless: false })
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    console.log('A aceder website...')
+    await page.goto('https://www.net-empregos.com/');
+    
+    //Remove as perguntas iniciais ao abrir o site, cookies e notificações.
+    await page.getByRole('button', { name: 'Permitir seleção' }).click();
+    await page.getByRole('button', { name: 'Não Ativar' }).click();
+
+    //LISTAR INFORMAÇÕES E REQUISITAR INPUT DO USER
+    //Locators de forms importantes
+
+    const {chave,zonaEscolhida,categoriaEscolhida} = await definirPesquisa(page);
+   
+
+    await page.getByRole('button', { name: ' Pesquisar' }).click();
+
+
+    //Pega os links da pagina inical
+    
+    let linkVagas = [...await pegarLinks(page)]
+
+    //pega o texto da paginação e a quantidade de vagas para descobrir quantas paginas foram retornadas
+    const frasePaginacao = await page.getByRole('heading', { name: 'ofertas encontradas' }).textContent()
+
+    //A frase de paginação só inclui este caractere caso existam multiplas paginas
+
+    if(frasePaginacao.includes('-')){
+        
+        let totalPaginas = frasePaginacao.split('-')[1].split(' ')[4];
+        
+
+        let linksPorPagina = await pegaLinksPorPagina(context,chave,categoriaEscolhida,zonaEscolhida, totalPaginas);
+        
+        if(linksPorPagina.length>=1) {
+            linkVagas.push(...linksPorPagina)
+        }
+    }
+    else {
+        console.log('Somente uma pagina foi encontrada');
+        
+    }
+
+    
+    //Faz um loop pelas paginas e coleta os links
+
+   
+    
+    console.log('Links coletados:', linkVagas);
+
+    console.log('Total de links coletados:', linkVagas.length); 
+
+    //Isto sera feito por vaga...
+    await acederVagasEGravarConteudos(context,linkVagas);
+
     await browser.close()
+
     return;
 }
 
